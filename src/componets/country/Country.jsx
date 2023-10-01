295fe104f0972844c5cef7ec392d1d71fbb0ff6e
mohammodjuwelis <mohammodjuwelislam@gmail.com>
2023-10-02 00:52:20 +0600

    clickHandler function addeds

--- src/componets/country/Country.jsx
+++ src/componets/country/Country.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
-    console.log(country)
-    const {name, flags} = country;
+
+    const [visited, setVisited] = useState(false);
+    const clickHandler = () => {
+        setVisited(!visited)
+    }
+
+    const {name, flags, area, languages} = country;
     // console.log(name)
     return (
         <div className='country-box'>
             <h3>Country Name: {name?.common}</h3>
-            <img src={flags.png} alt="" />
+            <img src={flags?.png} alt="" />
+            <h4>Area: {area}</h4>
+            <h3>Languages Name: {languages?.eng ? languages.eng : name?.common + " I don't know languages name?"}</h3>
+            <button onClick={clickHandler}>{visited ? 'Visited': 'Not Visit'}</button>
+            {visited ? 'I have to visited' : 'I want to going'}
         </div>
     );
 };
